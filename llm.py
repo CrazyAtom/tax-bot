257c0d7787431a9ffb59f0489d57722ad8cdcc43
crazyatom <crazyatom84@gmail.com>
2025-04-07 13:55:29 +0900

    refactoring get_history_retriever

--- llm.py
+++ llm.py
@@ -29,6 +29,33 @@ def get_retriever():
     return retriever
 
 
+def get_history_retriever():
+    llm = get_llm()
+    retriever = get_retriever()
+
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is."
+    )
+
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    return history_aware_retriever
+
+
 def get_llm(model='gpt-4o'):
     llm = ChatOpenAI(model=model)
     return llm
@@ -53,27 +80,7 @@ def get_dictionary_chain():
 
 def get_rag_chain():
     llm = get_llm()
-    retriever = get_retriever()
-
-    contextualize_q_system_prompt = (
-        "Given a chat history and the latest user question "
-        "which might reference context in the chat history, "
-        "formulate a standalone question which can be understood "
-        "without the chat history. Do NOT answer the question, "
-        "just reformulate it if needed and otherwise return it as is."
-    )
-
-    contextualize_q_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", contextualize_q_system_prompt),
-            MessagesPlaceholder("chat_history"),
-            ("human", "{input}"),
-        ]
-    )
-
-    history_aware_retriever = create_history_aware_retriever(
-        llm, retriever, contextualize_q_prompt
-    )
+    history_aware_retriever = get_history_retriever()
 
     qa_system_prompt = (
         "당신은 소득세법 전문가입니다. 사용자의 소득세법에 관한 질문에 답변해주세요"
